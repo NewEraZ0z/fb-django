adf367009b636b7d65f9e53c209df50dc6b3d79a
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-14 21:13:41 +0100

    Update urls.py

--- fb_yomamabot/urls.py
+++ fb_yomamabot/urls.py
@@ -1,7 +1,12 @@
 # yomamabot/fb_yomamabot/urls.py
 from django.urls import path
-from .views import YoMamaBotView
+from . import  views
+
 
 urlpatterns = [
-    path('webhook', YoMamaBotView.as_view(), name='webhook'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('webhook', views.YoMamaBotView.as_view(), name='webhook'),
+    path('conversations/', views.get_conversations, name='get_conversations'),
+    path('messages/<str:conversation_id>/', views.get_messages, name='get_messages'),
+    path('message/<str:message_id>/', views.get_message_details, name='get_message_details'),
 ]
