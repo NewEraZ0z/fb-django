2a4060b0195b8f68e3df114694f7b835c6695a86
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-14 21:03:44 +0100

    Update settings.py

--- yomamabot/settings.py
+++ yomamabot/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
     
 ]
+
+ASGI_APPLICATION = 'yomamabot.asgi.application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
