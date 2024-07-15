fef323082d077bf3119807994201b704ababccc1
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-15 19:51:22 +0100

    Update views.py

--- fb_yomamabot/views.py
+++ fb_yomamabot/views.py
@@ -1,80 +1,132 @@
-import json
+# views.py
 import requests
-from pprint import pprint
-from django.http import JsonResponse, HttpResponse
-from django.utils.decorators import method_decorator
-from django.views import generic
-from django.views.decorators.csrf import csrf_exempt
-from asgiref.sync import async_to_sync
-from channels.layers import get_channel_layer
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.http import JsonResponse, HttpResponse
+from django.views import View
+import json
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 def dashboard(request):
     return render(request, 'dashboard.html')
 
-class YoMamaBotView(generic.View):
+
+class YoMamaBotView(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
     def get(self, request, *args, **kwargs):
         hub_verify_token = request.GET.get('hub.verify_token')
         hub_challenge = request.GET.get('hub.challenge')
-        if hub_verify_token == '6698547781455':
+        if hub_verify_token == '6698547781455':  # Replace with your token
             return HttpResponse(hub_challenge, status=200)
         else:
             return HttpResponse('Error, invalid token', status=403)
 
-    @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
-
     def post(self, request, *args, **kwargs):
         incoming_message = json.loads(request.body.decode('utf-8'))
-        for entry in incoming_message['entry']:
-            for message in entry['messaging']:
+        for entry in incoming_message.get('entry', []):
+            for message in entry.get('messaging', []):
                 if 'message' in message:
-                    pprint(message)
+                    sender_id = message['sender']['id']
+                    message_text = message['message'].get('text')
+
+                    # Broadcast message to Django Channels group
                     channel_layer = get_channel_layer()
                     async_to_sync(channel_layer.group_send)(
-                        'chat_room1',  # dynamically generate based on user/page
+                        f"user_{sender_id}",  # Unique group for each user
                         {
-                            'type': 'chat_message',
-                            'message': message['message']['text']
+                            "type": "chat.message",
+                            "message": message_text,
+                            "sender_id": sender_id
                         }
                     )
-        return HttpResponse(status=200)
+        return JsonResponse({"status": "success"}, status=200)
 
-def get_conversations(request):
-    page_id = "YOUR_PAGE_ID"
-    access_token = "YOUR_PAGE_ACCESS_TOKEN"
-    platform = request.GET.get('platform', 'messenger')
-    
-    url = f"https://graph.facebook.com/v12.0/{page_id}/conversations"
-    params = {
-        'platform': platform,
-        'access_token': access_token
-    }
-    response = requests.get(url, params=params)
-    return JsonResponse(response.json())
 
-def get_messages(request, conversation_id):
-    access_token = "YOUR_PAGE_ACCESS_TOKEN"
-    
-    url = f"https://graph.facebook.com/v12.0/{conversation_id}"
-    params = {
-        'fields': 'messages',
-        'access_token': access_token
-    }
-    response = requests.get(url, params=params)
-    return JsonResponse(response.json())
 
-def get_message_details(request, message_id):
-    access_token = "YOUR_PAGE_ACCESS_TOKEN"
+
+
+# import json
+# import requests
+# from pprint import pprint
+# from django.http import JsonResponse, HttpResponse
+# from django.utils.decorators import method_decorator
+# from django.views import generic
+# from django.views.decorators.csrf import csrf_exempt
+# from asgiref.sync import async_to_sync
+# from channels.layers import get_channel_layer
+# from django.shortcuts import render
+
+# def dashboard(request):
+#     return render(request, 'dashboard.html')
+
+# class YoMamaBotView(generic.View):
+#     def get(self, request, *args, **kwargs):
+#         hub_verify_token = request.GET.get('hub.verify_token')
+#         hub_challenge = request.GET.get('hub.challenge')
+#         if hub_verify_token == '6698547781455':
+#             return HttpResponse(hub_challenge, status=200)
+#         else:
+#             return HttpResponse('Error, invalid token', status=403)
+
+#     @method_decorator(csrf_exempt)
+#     def dispatch(self, request, *args, **kwargs):
+#         return super().dispatch(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         incoming_message = json.loads(request.body.decode('utf-8'))
+#         for entry in incoming_message['entry']:
+#             for message in entry['messaging']:
+#                 if 'message' in message:
+#                     pprint(message)
+#                     channel_layer = get_channel_layer()
+#                     async_to_sync(channel_layer.group_send)(
+#                         'chat_room1',  # dynamically generate based on user/page
+#                         {
+#                             'type': 'chat_message',
+#                             'message': message['message']['text']
+#                         }
+#                     )
+#         return HttpResponse(status=200)
+
+# def get_conversations(request):
+#     page_id = "YOUR_PAGE_ID"
+#     access_token = "YOUR_PAGE_ACCESS_TOKEN"
+#     platform = request.GET.get('platform', 'messenger')
     
-    url = f"https://graph.facebook.com/v12.0/{message_id}"
-    params = {
-        'fields': 'id,created_time,from,to,message',
-        'access_token': access_token
-    }
-    response = requests.get(url, params=params)
-    return JsonResponse(response.json())
+#     url = f"https://graph.facebook.com/v12.0/{page_id}/conversations"
+#     params = {
+#         'platform': platform,
+#         'access_token': access_token
+#     }
+#     response = requests.get(url, params=params)
+#     return JsonResponse(response.json())
+
+# def get_messages(request, conversation_id):
+#     access_token = "YOUR_PAGE_ACCESS_TOKEN"
+    
+#     url = f"https://graph.facebook.com/v12.0/{conversation_id}"
+#     params = {
+#         'fields': 'messages',
+#         'access_token': access_token
+#     }
+#     response = requests.get(url, params=params)
+#     return JsonResponse(response.json())
+
+# def get_message_details(request, message_id):
+#     access_token = "YOUR_PAGE_ACCESS_TOKEN"
+    
+#     url = f"https://graph.facebook.com/v12.0/{message_id}"
+#     params = {
+#         'fields': 'id,created_time,from,to,message',
+#         'access_token': access_token
+#     }
+#     response = requests.get(url, params=params)
+#     return JsonResponse(response.json())
 
 
 
