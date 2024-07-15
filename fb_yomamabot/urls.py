5f847d1e4b29040a272cce0c899e6c0f25a85f66
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-15 20:25:45 +0100

    Update urls.py

--- fb_yomamabot/urls.py
+++ fb_yomamabot/urls.py
@@ -6,6 +6,7 @@ from . import  views
 urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
     path('webhook', views.YoMamaBotView.as_view(), name='webhook'),
+    path('send_message/', views.send_message, name='send_message'),
     path('conversations/', views.get_conversations, name='get_conversations'),
     path('messages/<str:conversation_id>/', views.get_messages, name='get_messages'),
     path('message/<str:message_id>/', views.get_message_details, name='get_message_details'),
