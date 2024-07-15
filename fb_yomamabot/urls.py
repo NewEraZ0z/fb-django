2795830b025435b42d3f4e72f968ac412927c151
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-15 22:43:58 +0100

    Delete fb_yomamabot directory

--- fb_yomamabot/urls.py
+++
@@ -1,9 +0,0 @@
-# yomamabot/fb_yomamabot/urls.py
-from django.urls import path
-from . import  views
-
-
-urlpatterns = [
-    path('dashboard', views.dashboard, name='dashboard'),
-    path('webhook', views.YoMamaBotView.as_view(), name='webhook'),
-]
