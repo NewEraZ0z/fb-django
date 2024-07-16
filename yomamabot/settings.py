8225f8f458831597c8bdf958fd80ab25b00fc035
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-16 11:00:41 +0100

    Update settings.py

--- yomamabot/settings.py
+++ yomamabot/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'yomamabot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'fb_yomamabot', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
