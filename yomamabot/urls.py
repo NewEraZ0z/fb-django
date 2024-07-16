e9daaa22636d30ef8e970e48df123527bbe651b2
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-16 17:17:20 +0100

    Delete yomamabot directory

--- yomamabot/urls.py
+++
@@ -1,8 +0,0 @@
-# yomamabot/yomamabot/urls.py
-from django.urls import path, re_path, include
-from django.contrib import admin
-
-urlpatterns = [
-    re_path(r'^admin/', admin.site.urls),
-    path('', include('fb_yomamabot.urls')),
-]
