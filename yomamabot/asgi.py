945fded672d2d02b4a25ddd65370b8456de1c01f
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-16 21:49:21 +0100

    Update asgi.py

--- yomamabot/asgi.py
+++ yomamabot/asgi.py
@@ -27,35 +27,3 @@ application = ProtocolTypeRouter({
 
 
 
-
-
-
-
-# # asgi.py or routing.py
-# import os
-# from django.core.asgi import get_asgi_application
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from channels.auth import AuthMiddlewareStack
-# import fb_yomamabot.routing  # Replace with your app name
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yomamabot.settings')
-
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-#     "websocket": AuthMiddlewareStack(
-#         URLRouter(
-#             fb_yomamabot.routing.websocket_urlpatterns
-#         )
-#     ),
-# })
-
-
-
-
-# import os
-
-# from django.core.asgi import get_asgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yomamabot.settings')
-
-# application = get_asgi_application()
