285492173f99c08dcc9b16295900abbd4695593d
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-16 15:42:30 +0100

    Update wsgi.py

--- yomamabot/wsgi.py
+++ yomamabot/wsgi.py
@@ -1,16 +1,36 @@
-"""
-WSGI config for yomamabot project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
-
-from django.core.wsgi import get_wsgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+import fb_yomamabot.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yomamabot.settings')
 
-application = get_wsgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            your_app.routing.websocket_urlpatterns
+        )
+    ),
+})
+
+
+
+
+# """
+# WSGI config for yomamabot project.
+
+# It exposes the WSGI callable as a module-level variable named ``application``.
+
+# For more information on this file, see
+# https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+# """
+
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yomamabot.settings')
+
+# application = get_wsgi_application()
