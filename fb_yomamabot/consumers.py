8097ca92883ba04d17465852db5711c9a3d40bb3
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-16 16:42:05 +0100

    Update consumers.py

--- fb_yomamabot/consumers.py
+++ fb_yomamabot/consumers.py
@@ -1,35 +1,47 @@
-import json
+# fb_yomamabot/consumers.py
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync  # Add this import
+import json
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        self.group_name = "chat_group"
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # Join room group
         async_to_sync(self.channel_layer.group_add)(
-            self.group_name,
+            self.room_group_name,
             self.channel_name
         )
+
         self.accept()
 
     def disconnect(self, close_code):
+        # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
-            self.group_name,
+            self.room_group_name,
             self.channel_name
         )
 
+    # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    # Receive message from room group
+    def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
         self.send(text_data=json.dumps({
             'message': message
         }))
-
-    def chat_message(self, event):
-        message = event['message']
-        sender_id = event['sender_id']
-
-        self.send(text_data=json.dumps({
-            'message': message,
-            'sender_id': sender_id,
-        }))
-
