42455fb150b9280d0aabad667599a69131496a66
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-16 20:21:35 +0100

    Update consumers.py

--- fb_yomamabot/consumers.py
+++ fb_yomamabot/consumers.py
@@ -1,17 +1,57 @@
-# yomamabot/fb_yomamabot/consumers.py
+# # yomamabot/fb_yomamabot/consumers.py
+
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
-
-    async def disconnect(self, close_code):
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+    
+    def disconnect(self, close_code):
         pass
+    
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
 
-    async def receive(self, text_data):
-        message = json.loads(text_data)
-        # Handle incoming message here, e.g., save to database or process
-        await self.send(text_data=json.dumps({
+        # Broadcast the message to the group
+        async_to_sync(self.channel_layer.group_send)(
+            'chat_group',
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    def chat_message(self, event):
+        message = event['message']
+        
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
             'message': message
         }))
+
+
+
+
+
+
+
+
+# import json
+# from channels.generic.websocket import AsyncWebsocketConsumer
+
+# class ChatConsumer(AsyncWebsocketConsumer):
+#     async def connect(self):
+#         await self.accept()
+
+#     async def disconnect(self, close_code):
+#         pass
+
+#     async def receive(self, text_data):
+#         message = json.loads(text_data)
+#         # Handle incoming message here, e.g., save to database or process
+#         await self.send(text_data=json.dumps({
+#             'message': message
+#         }))
