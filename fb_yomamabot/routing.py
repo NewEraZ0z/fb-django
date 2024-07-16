cad5c0c360a02a31acddb4cce125fcb9c6bf3683
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-16 21:48:23 +0100

    Update routing.py

--- fb_yomamabot/routing.py
+++ fb_yomamabot/routing.py
@@ -7,11 +7,3 @@ websocket_urlpatterns = [
 ]
 
 
-# # yomamabot/fb_yomamabot/routing.py
-# from django.urls import re_path
-
-# from . import consumers
-
-# websocket_urlpatterns = [
-#     re_path(r'ws/chat/$', consumers.ChatConsumer.as_asgi()),
-# ]
