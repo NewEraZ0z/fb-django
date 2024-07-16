17bddd9a9ecb011a34c5b32fce627d430052cf89
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-16 21:10:14 +0100

    Update asgi.py

--- yomamabot/asgi.py
+++ yomamabot/asgi.py
@@ -6,24 +6,48 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
-
-# asgi.py or routing.py
+# fb_yomamabot/asgi.py
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import fb_yomamabot.routing  # Replace with your app name
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from fb_yomamabot.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yomamabot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fb_yomamabot.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            fb_yomamabot.routing.websocket_urlpatterns
+            websocket_urlpatterns
         )
     ),
 })
+
+
+
+
+
+
+
+
+# # asgi.py or routing.py
+# import os
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# import fb_yomamabot.routing  # Replace with your app name
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yomamabot.settings')
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             fb_yomamabot.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
 
 
 
