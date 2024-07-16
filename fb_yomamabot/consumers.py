eb478e5106b27757641fb00093dc4a0c5ef42418
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-16 15:45:40 +0100

    Update consumers.py

--- fb_yomamabot/consumers.py
+++ fb_yomamabot/consumers.py
@@ -3,10 +3,18 @@ from channels.generic.websocket import WebsocketConsumer
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.group_name = "chat_group"
+        async_to_sync(self.channel_layer.group_add)(
+            self.group_name,
+            self.channel_name
+        )
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(
+            self.group_name,
+            self.channel_name
+        )
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -15,3 +23,13 @@ class ChatConsumer(WebsocketConsumer):
         self.send(text_data=json.dumps({
             'message': message
         }))
+
+    def chat_message(self, event):
+        message = event['message']
+        sender_id = event['sender_id']
+
+        self.send(text_data=json.dumps({
+            'message': message,
+            'sender_id': sender_id,
+        }))
+
