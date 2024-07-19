4f393e55074daac469918d931b57d286aa01fc31
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-19 14:10:08 +0100

    Update urls.py

--- fb_yomamabot/urls.py
+++ fb_yomamabot/urls.py
@@ -1,6 +1,6 @@
 # yomamabot/fb_yomamabot/urls.py
 from django.urls import path
-from .views import YoMamaBotView, chat_widget, send_message, fetch_pages
+from .views import YoMamaBotView, chat_widget, send_message, fetch_pages, fetch_users
 
 
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('webhook', YoMamaBotView.as_view(), name='webhook'),
     path('chat_widget', chat_widget, name='chat_widget'),
     path('fetch_pages/', fetch_pages, name='fetch_pages'),
+     path('fetch_users/', fetch_users, name='fetch_users'),
     path('send_message/', send_message, name='send_message'),
 ]
 
