17d2e4d44b371b11574a9f9877ef7f40c1f4462a
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-19 22:46:58 +0100

    Delete fb_yomamabot directory

--- fb_yomamabot/routing.py
+++
@@ -1,9 +0,0 @@
-# fb_yomamabot/routing.py
-from django.urls import re_path
-from .consumers import ChatConsumer
-
-websocket_urlpatterns = [
-    re_path(r'ws/chat/$', ChatConsumer.as_asgi()),
-]
-
-
