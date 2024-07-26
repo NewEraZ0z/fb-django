3b39afa55e878612bbeaebc8b0b02b9ef6985248
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-07-26 10:22:07 +0100

    Update urls.py

--- fb_yomamabot/urls.py
+++ fb_yomamabot/urls.py
@@ -6,7 +6,6 @@ InstagHook(View)
 
 urlpatterns = [
     path('webhook', YoMamaBotView.as_view(), name='webhook'),
-    path('instahook', InstagHook.as_view(), name='instahook'),
     path('chat_widget', chat_widget, name='chat_widget'),
     path('fetch_pages/', fetch_pages, name='fetch_pages'),
      path('fetch_users/', fetch_users, name='fetch_users'),
